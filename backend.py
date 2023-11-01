2d9b125ada2a3d907fbd2376cc88a5921dffe302
kanakof <kanakofukuda199531@gmail.com>
2023-11-02 00:02:42 +0900

    feat: backend.py整備

--- backend.py
+++ backend.py
@@ -6,10 +6,17 @@ app = Flask(__name__, static_url_path='',
             static_folder='static',
             template_folder='templates')
 
-
 @app.route('/', methods=['GET'])
 def process_GET_request():
-    return render_template('front.html', title='personality', name="top page")
+    return render_template('page.html', title='personality', name="top page")
+
+@app.route('/works-template.html', methods=['GET'])
+def work_template1():
+    return render_template('works-template.html', title='personality', name="top page")
+
+@app.route('/works-template2.html', methods=['GET'])
+def work_template2():
+    return render_template('works-template2.html', title='personality', name="top page")
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000, threaded=True)
